4e0fa77874aee4cdb4d058a93979dafeb87688cd
Carter Williams <135449037+CarterWilliams2@users.noreply.github.com>
2026-03-17 10:38:31 -0500

    write try-except blocks to open pdf files

--- ssp-project.py
+++ ssp-project.py
@@ -1,6 +1,20 @@
 # Main file for our project
+from pypdf import PdfReader
+from pypdf.errors import PdfReadError
 
 # input validation function
 # takes two pdfs and applies adequate input validation measures
 def validate_inpute_files(file1, file2):
+    # try to open file1
+    try:
+        PdfReader(file1)
+    except:
+        PdfReadError("Invalid first PDF file")
+    
+    #try to open file2
+    try:
+        PdfReader(file2)
+    except:
+        PdfReadError("Invalid second PDF file")
+    
     return None
