fc1aa8bb9c3df692e50fafc330c0c452795bfdd6
Carter Williams <135449037+CarterWilliams2@users.noreply.github.com>
2026-03-17 10:28:16 -0500

    make skeleton of validate_input_files

--- ssp-project.py
+++ ssp-project.py
@@ -1,1 +1,6 @@
 # Main file for our project
+
+# input validation function
+# takes two pdfs and applies adequate input validation measures
+def validate_inpute_files(file1, file2):
+    return None
