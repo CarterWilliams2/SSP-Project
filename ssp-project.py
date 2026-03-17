964b093fee3e47fca2b1b50a39e8ac1e24e67d38
Carter Williams <135449037+CarterWilliams2@users.noreply.github.com>
2026-03-17 10:50:46 -0500

    test input validation with two real pdfs

--- ssp-project.py
+++ ssp-project.py
@@ -1,20 +1,27 @@
 # Main file for our project
 from pypdf import PdfReader
 from pypdf.errors import PdfReadError
+import warnings
 
 # input validation function
 # takes two pdfs and applies adequate input validation measures
 def validate_inpute_files(file1, file2):
+    errors = 0
+    
     # try to open file1
     try:
         PdfReader(file1)
     except:
         PdfReadError("Invalid first PDF file")
+        errors += 1
     
     #try to open file2
     try:
         PdfReader(file2)
     except:
         PdfReadError("Invalid second PDF file")
+        errors += 1
     
-    return None
+    print(errors, " error(s)")
+
+validate_inpute_files("./pdf-inputs/cis-r1.pdf", "./pdf-inputs/cis-r2.pdf")
