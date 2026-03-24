ec1083b8d883c04ad34d0f69b6a1a0d18c64adf6
Carter Williams <135449037+CarterWilliams2@users.noreply.github.com>
2026-03-24 17:00:38 -0500

    create test for few shot

--- test_ssp_project.py
+++ test_ssp_project.py
@@ -1,5 +1,5 @@
 import unittest
-from ssp_project import validate_input_files, construct_zero_shot_prompt
+from ssp_project import validate_input_files, construct_zero_shot_prompt, construct_few_shot_prompt
 
 
 class TestTask1Methods(unittest.TestCase):
@@ -17,6 +17,13 @@ class TestTask1Methods(unittest.TestCase):
         
         zero_shot_prompt = "Please analyze the following two documents to identify key data elements: file1.pdf and file2.pdf. Structure your output as a nested dictionary with the following structure: {element1: {name: '', requirements: [req1, req2, req3]}, element2: {name: '', requirements: [req1, req2]} }" 
         assert zero_shot_prompt == construct_zero_shot_prompt(file1, file2)
+    
+    def test_few_shot_prompt(self):
+        file1 = "file1.pdf"
+        file2 = "file2.pdf"
+        
+        few_shot_prompt = "Please analyze the following two documents to identify key data elements: file1.pdf and file2.pdf. Structure your output as a nested dictionary with the following structure: {element1: {name: '', requirements: [req1, req2, req3]}, element2: {name: '', requirements: [req1, req2]} }. For example: element1: {name: 'title', requirements: ['human-readable', 'descriptive']}}. Also could be: element2: {name: 'rationale', requirements: ['sound reasoning', 'concise']}"
+        assert few_shot_prompt == construct_few_shot_prompt(file1, file2) 
     
     
     
